4f68762f0db169db9ae10855bb50839b33944ce1
Tom Dean <tom@dean33.com>
2025-04-24 10:08:52 -0500

    Added a second cluster for federation use cases.

--- k3d-sandboxes/gloo-gw-sandboxes/scripts/cluster-destroy-k3d.sh
+++ k3d-sandboxes/gloo-gw-sandboxes/scripts/cluster-destroy-k3d.sh
@@ -2,7 +2,7 @@
 # cluster-destroy-k3d.sh
 # Automates cluster deletion and cleans up the gloo-gw kubectl context
 # Tom Dean
-# Last edit: 4/23/2025
+# Last edit: 4/24/2025
 
 # Set environment variables
 
@@ -10,12 +10,14 @@ source vars.sh
 
 # Remove the k3d cluster
 
-k3d cluster delete $CLUSTER_NAME
+k3d cluster delete $CLUSTER1_NAME
+k3d cluster delete $CLUSTER2_NAME
 k3d cluster list
 
 # Remove the kubectl context
 
-kubectx -d $KUBECTX_NAME
+kubectx -d $KUBECTX_NAME1
+kubectx -d $KUBECTX_NAME2
 kubectx
 
 echo "Cluster deleted!"
--- k3d-sandboxes/gloo-gw-sandboxes/vars.sh
+++ k3d-sandboxes/gloo-gw-sandboxes/vars.sh
@@ -2,11 +2,14 @@
 # Environment variables for the k3d sandbox
 #
 # Tom Dean
-# Last edit: 4/23/2025
+# Last edit: 4/24/2025
 #
 # Set variables here and then execute or source script
 # Do this before executing any sandbox scripts
 
-export CLUSTER_NAME=gloo-gw-playground
-export KUBECTX_NAME=gloo-gw
+export CLUSTER_NETWORK=gloo-network
+export CLUSTER1_NAME=cluster1
+export CLUSTER2_NAME=cluster2
+export KUBECTX_NAME1=cluster1
+export KUBECTX_NAME2=cluster2
 export LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
