76132fe10163cf333ace16fc4ffd56bdfbdc0265
Tom Dean <tom@dean33.com>
2025-04-25 09:50:14 -0500

    More work on cluster scripts.  Set number of clusters as a variable in vars.sh, use prefix strings for cluster and kubectx names.

--- k3d-sandboxes/gloo-gw-sandboxes/vars.sh
+++ k3d-sandboxes/gloo-gw-sandboxes/vars.sh
@@ -1,17 +1,14 @@
 # vars.sh
-# Environment variables for the k3d sandbox
+# Environment variables for the sandbox
 #
 # Tom Dean
-# Last edit: 4/24/2025
+# Last edit: 4/25/2025
 #
 # Set variables here and then execute or source script
 # Do this before executing any sandbox scripts
 
+export NUM_CLUSTERS=03  # Express this as two digits, padded with a zero if necessary!
+export CLUSTER_NAME_PREFIX=gloo-
+export KUBECTX_NAME_PREFIX=gloo-
 export CLUSTER_NETWORK=gloo-network
-export CLUSTER1_NAME=cluster1
-export CLUSTER2_NAME=cluster2
-export CLUSTER3_NAME=cluster3
-export KUBECTX_NAME1=cluster1
-export KUBECTX_NAME2=cluster2
-export KUBECTX_NAME3=cluster3
 export LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
