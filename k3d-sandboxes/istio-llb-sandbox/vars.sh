fc91269ab49892b0e23d973f559b5a45a8dc1167
Tom Dean <tdean@d2iq.com>
2025-04-29 19:29:13 -0500

    Added isio-llb-sandbox.

---
+++ k3d-sandboxes/istio-llb-sandbox/vars.sh
@@ -0,0 +1,14 @@
+# vars.sh
+# Environment variables for the sandbox
+#
+# Tom Dean
+# Last edit: 4/29/2025
+#
+# Set variables here and then execute or source script
+# Do this before executing any sandbox scripts
+
+export NUM_CLUSTERS=1
+export CLUSTER_NAME_PREFIX=istio-llb-
+export KUBECTX_NAME_PREFIX=istio-llb-
+export CLUSTER_NETWORK=istio-network
+export LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
