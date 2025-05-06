e9492c531aed7539de04eab2af4e53bc7c365792
Tom Dean <tom@dean33.com>
2025-05-06 11:02:55 -0500

    Added gme-llb-sandbox.

---
+++ k3d-sandboxes/gme-llb-sandbox/README.md
@@ -0,0 +1,1 @@
+# gme-llb-sandboxes
---
+++ k3d-sandboxes/gme-llb-sandbox/vars.sh
@@ -0,0 +1,14 @@
+# vars.sh
+# Environment variables for the sandbox
+#
+# Tom Dean
+# Last edit: 5/6/2025
+#
+# Set variables here and then execute or source script
+# Do this before executing any sandbox scripts
+
+export NUM_CLUSTERS=1
+export CLUSTER_NAME_PREFIX=gme-llb-
+export KUBECTX_NAME_PREFIX=gme-llb-
+export CLUSTER_NETWORK=gme-network
+export GLOO_MESH_LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
