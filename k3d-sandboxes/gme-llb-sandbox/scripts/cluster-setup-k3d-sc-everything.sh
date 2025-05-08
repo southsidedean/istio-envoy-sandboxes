cb14367ab857835fb6e8c16cb6f03af5025d3a18
Tom Dean <tdean@d2iq.com>
2025-05-08 16:59:01 -0500

    More work on ingress.

--- k3d-sandboxes/gme-llb-sandbox/scripts/cluster-setup-k3d-sc-everything.sh
+++ k3d-sandboxes/gme-llb-sandbox/scripts/cluster-setup-k3d-sc-everything.sh
@@ -111,10 +111,10 @@ helm install grafana -n grafana --create-namespace grafana/grafana \
 
 # Create ingress for UI and Grafana
 
-#kubectl apply -f manifests/grafana-ext-svc.yaml
-#kubectl apply -f manifests/cluster-ingress.yaml
-kubectl apply -f manifests/grafana-ingress.yaml
-kubectl apply -f manifests/gloo-mesh-ui-ingress.yaml
+kubectl apply -f manifests/grafana-ext-svc.yaml
+kubectl apply -f manifests/cluster-ingress.yaml
+#kubectl apply -f manifests/grafana-ingress.yaml
+#kubectl apply -f manifests/gloo-mesh-ui-ingress.yaml
 
 # Start the Gloo Dashboard in the background and suppress output
 
