281fca325768bff5f23c3479265b4272a5a4c668
Tom Dean <tom@dean33.com>
2025-05-23 08:43:38 -0500

    Added GLOOCTL_VERSION and GLOO_VERSION to vars.sh.

--- k3d-sandboxes/gloo-gw-sandbox/vars.sh
+++ k3d-sandboxes/gloo-gw-sandbox/vars.sh
@@ -2,7 +2,7 @@
 # Environment variables for the sandbox
 #
 # Tom Dean
-# Last edit: 4/25/2025
+# Last edit: 5/23/2025
 #
 # Set variables here and then execute or source script
 # Do this before executing any sandbox scripts
@@ -12,4 +12,6 @@ export CLUSTER_NAME_PREFIX=gloo-
 export KUBECTX_NAME_PREFIX=gloo-
 export CLUSTER_NETWORK=gloo-network
 export GLOO_NAMESPACE=gloo-system
+export GLOOCTL_VERSION=1.18.17
+export GLOO_VERSION=1.18.17
 export LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
