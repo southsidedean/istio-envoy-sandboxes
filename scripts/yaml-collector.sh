8694e12db8756831ef5396069c3a61e93076326d
Tom Dean <tom@dean33.com>
2025-06-03 16:30:48 -0500

    Updates to yaml-collector.sh script.

--- scripts/yaml-collector.sh
+++ scripts/yaml-collector.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 # Script to collect manifests for a bunch of k8s objects across all namespaces and create a zipfile
-# Grabs Solo api-resources
 # Tom Dean
 # Last edit: 6/3/2025
 
-# Set object list
+# Set object list - what are we collecting?
+# Use the OBJECTS variable to set this
 
+# Fixed list - in this example services, deployments and pods
+#OBJECTS="svc deploy pods"
+
+# All objects on the cluster
+#OBJECTS=$(kubectl api-resources | grep -v NAME | awk {'print $1'})
+
+# All Solo objects
 OBJECTS=$(kubectl api-resources | grep -i solo | grep -v NAME | awk {'print $1'})
 
 # Create directory to collect all manifests
