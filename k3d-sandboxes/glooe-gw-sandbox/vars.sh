71197537d6bcbe7ea9bf0b80ea45e959efa6f023
Tom Dean <tom@dean33.com>
2025-06-25 10:46:57 -0500

    Added new k3d sandbox - glooe-gw-sandbox, cloned from gloo-gw-sandbox.

---
+++ k3d-sandboxes/glooe-gw-sandbox/vars.sh
@@ -0,0 +1,17 @@
+# vars.sh
+# Environment variables for the sandbox
+#
+# Tom Dean
+# Last edit: 5/23/2025
+#
+# Set variables here and then execute or source script
+# Do this before executing any sandbox scripts
+
+export NUM_CLUSTERS=3
+export CLUSTER_NAME_PREFIX=gloo-
+export KUBECTX_NAME_PREFIX=gloo-
+export CLUSTER_NETWORK=gloo-network
+export GLOO_NAMESPACE=gloo-system
+export GLOOCTL_VERSION=1.18.17
+export GLOO_VERSION=1.18.17
+export LICENSE_KEY=<<INSERT_LICENSE_STRING_HERE>>
